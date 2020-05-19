783eb76539ef2113d05d8dad6260bd10b5902ede
ftorresma <38130303+ftorresma@users.noreply.github.com>
2020-05-19 15:07:29 -0500

    estructura
    botones y redireccion a cada modo

--- Matplot/matdjango/urls.py
+++ Matplot/matdjango/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
+    path('ecualizar',views.funct1),
+    path('contrastar',views.funct2),
+    path('logaritmear',views.funct3),
+    path('exponenciar',views.funct4),
+    path('home',views.home),
 ]
--- Matplot/matdjango/views.py
+++ Matplot/matdjango/views.py
@@ -28,4 +28,106 @@ def home(request):
     buf.seek(0)
     string = base64.b64encode(buf.read())
     uri = urllib.parse.quote(string)
+
     return render(request,'home.html',{'data':uri})
+
+def funct1(request):
+    img=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    img2=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    fils,cols=img.shape
+    p=[0]*256
+    c=70
+    for x in range(fils):
+        for y in range(cols):
+            p=c*math.log(1+img.item(x,y),10)
+            if p>255:
+                p=255
+            if p<0:
+                p=0
+            img2.itemset((x,y),int(p))
+    plt.imshow(img2,'gray')
+    #plt.plot(range(10))
+    fig =plt.gcf()
+
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+
+    return render(request,'ecualizacion.html')
+def funct2(request):
+    img=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    img2=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    fils,cols=img.shape
+    p=[0]*256
+    c=70
+    for x in range(fils):
+        for y in range(cols):
+            p=c*math.log(1+img.item(x,y),10)
+            if p>255:
+                p=255
+            if p<0:
+                p=0
+            img2.itemset((x,y),int(p))
+    plt.imshow(img2,'gray')
+    #plt.plot(range(10))
+    fig =plt.gcf()
+
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+
+    return render(request,'contrast.html')
+def funct3(request):
+    img=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    img2=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    fils,cols=img.shape
+    p=[0]*256
+    c=70
+    for x in range(fils):
+        for y in range(cols):
+            p=c*math.log(1+img.item(x,y),10)
+            if p>255:
+                p=255
+            if p<0:
+                p=0
+            img2.itemset((x,y),int(p))
+    plt.imshow(img2,'gray')
+    #plt.plot(range(10))
+    fig =plt.gcf()
+
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+
+    return render(request,'logarithm.html')
+def funct4(request):
+    img=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    img2=cv2.imread('matdjango/Imagenes/log_1.jpg', cv2.IMREAD_GRAYSCALE)
+    fils,cols=img.shape
+    p=[0]*256
+    c=70
+    for x in range(fils):
+        for y in range(cols):
+            p=c*math.log(1+img.item(x,y),10)
+            if p>255:
+                p=255
+            if p<0:
+                p=0
+            img2.itemset((x,y),int(p))
+    plt.imshow(img2,'gray')
+    #plt.plot(range(10))
+    fig =plt.gcf()
+
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    
+    return render(request,'exponencial.html')
