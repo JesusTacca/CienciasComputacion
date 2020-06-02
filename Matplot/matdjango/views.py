ddc3a76ed0cd326495528f7c3d9b683ca7aef724
JesusTaccaG <jtaccag@unsa.edu.pe>
2020-06-02 00:42:22 -0500

    asdsa

--- Matplot/matdjango/views.py
+++ Matplot/matdjango/views.py
@@ -566,7 +566,120 @@ def raisepower_custom(request):
 
     #actualiza la pagina de la funcion
     return render(request,'raisetopower.html',{'data':uri})
-
+def addition(request):
+    img=cv2.imread('matdjango/Imagenes/images/'+'add_2.jpg')
+    img2=cv2.imread('matdjango/Imagenes/images/'+'add_1.jpg')
+    a=max(len(img),len(img2))
+    b=max(len(img[0]),len(img2[0]))
+    img=cv2.resize(img,(b,a))
+    img2=cv2.resize(img2,(b,a))
+    for x in range(a):
+        for y in range(b):
+            p=int(img.item(x,y,0)/2+img2.item(x,y,0)/2)
+            img.itemset((x,y,0),p)
+            p=int(img.item(x,y,1)/2+img2.item(x,y,1)/2)
+            img.itemset((x,y,1),p)
+            p=int(img.item(x,y,2)/2+img2.item(x,y,2)/2)
+            img.itemset((x,y,2),p)
+    plt.imshow(img)
+    #plt.plot(range(10))
+    fig =plt.gcf()
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request,'addition.html',{'data':uri})
+def addition1(request):
+    img=cv2.imread('matdjango/Imagenes/images/'+'add_2.jpg')
+    img2=cv2.imread('matdjango/Imagenes/images/'+'add_1.jpg')
+    a=max(len(img),len(img2))
+    b=max(len(img[0]),len(img2[0]))
+    img=cv2.resize(img,(b,a))
+    img2=cv2.resize(img2,(b,a))
+    for x in range(a):
+        for y in range(b):
+            p=int(img.item(x,y,0)/2+img2.item(x,y,0)/2)
+            img.itemset((x,y,0),p)
+            p=int(img.item(x,y,1)/2+img2.item(x,y,1)/2)
+            img.itemset((x,y,1),p)
+            p=int(img.item(x,y,2)/2+img2.item(x,y,2)/2)
+            img.itemset((x,y,2),p)
+    plt.imshow(img)
+    #plt.plot(range(10))
+    fig =plt.gcf()
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request,'addition.html',{'data':uri})
+def subtraction(request):
+    img=cv2.imread('matdjango/Imagenes/images/'+'add_2.jpg')
+    img2=cv2.imread('matdjango/Imagenes/images/'+'add_1.jpg')
+    a=max(len(img),len(img2))
+    b=max(len(img[0]),len(img2[0]))
+    img=cv2.resize(img,(b,a))
+    img2=cv2.resize(img2,(b,a))
+    for x in range(a):
+        for y in range(b):
+            p=(img.item(x,y,0)-img2.item(x,y,0))
+            if p<0:
+                img.itemset((x,y,0),0)
+            else:
+                img.itemset((x,y,0),p)
+            p=(img.item(x,y,1)-img2.item(x,y,1))
+            if p<0:
+                img.itemset((x,y,1),0)
+            else:
+                img.itemset((x,y,1),p)
+            p=(img.item(x,y,2)-img2.item(x,y,2))
+            if p<0:
+                img.itemset((x,y,2),0)
+            else:
+                img.itemset((x,y,2),p)
+    plt.imshow(img)
+    #plt.plot(range(10))
+    fig =plt.gcf()
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request,'subtraction.html',{'data':uri})
+def subtraction1(request):
+    img=cv2.imread('matdjango/Imagenes/images/'+'add_2.jpg')
+    img2=cv2.imread('matdjango/Imagenes/images/'+'add_1.jpg')
+    a=max(len(img),len(img2))
+    b=max(len(img[0]),len(img2[0]))
+    img=cv2.resize(img,(b,a))
+    img2=cv2.resize(img2,(b,a))
+    for x in range(a):
+        for y in range(b):
+            p=(img.item(x,y,0)-img2.item(x,y,0))
+            if p<0:
+                img.itemset((x,y,0),0)
+            else:
+                img.itemset((x,y,0),p)
+            p=(img.item(x,y,1)-img2.item(x,y,1))
+            if p<0:
+                img.itemset((x,y,1),0)
+            else:
+                img.itemset((x,y,1),p)
+            p=(img.item(x,y,2)-img2.item(x,y,2))
+            if p<0:
+                img.itemset((x,y,2),0)
+            else:
+                img.itemset((x,y,2),p)
+    plt.imshow(img)
+    #plt.plot(range(10))
+    fig =plt.gcf()
+    buf=io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request,'subtraction.html',{'data':uri})
 def image_upload(request):
     if request.method == 'POST':
         form = HotelForm(request.POST, request.FILES)
