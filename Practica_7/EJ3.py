e4f63b1713f243352491dec75358aa42995a9b74
PaulSV123 <38119360+PaulSV123@users.noreply.github.com>
2020-06-05 00:29:30 -0500

    Ejercicio 3

--- Practica_7/EJ3.py
+++ Practica_7/EJ3.py
@@ -1,27 +1,56 @@
 import cv2
 import random
-import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-img=cv2.imread('perro.jpg')
-img2=cv2.imread('guerra.jpg')
+img=cv2.imread('sub_10.jpg')
+img2=cv2.imread('sub_11.jpg')
 a=max(len(img),len(img2))
 b=max(len(img[0]),len(img2[0]))
 img=cv2.resize(img,(b,a))
 img2=cv2.resize(img2,(b,a))
 
-C=0.7
+C=100
+A=0
+
+B=255
+c1=255
+d1=0
+c2=255
+d2=0
+c3=255
+d3=0
+for x in range(a):
+    for y in range(b):
+
+        p=(img.item(x,y,0)/img2.item(x,y,0))
+        img.itemset((x,y,0),p*C)
+        if c1>img.item(x,y,0):
+            c1=img.item(x,y,0)
+        if d1<img.item(x,y,0):
+            d1=img.item(x,y,0)
+        p=(img.item(x,y,1)/img2.item(x,y,1))
+        img.itemset((x,y,1),p*C)
+        if c2>img.item(x,y,1):
+            c2=img.item(x,y,1)
+        if d2<img.item(x,y,1):
+            d2=img.item(x,y,1)
+        p=(img.item(x,y,2)/img2.item(x,y,2))
+        img.itemset((x,y,2),p*C)
+        if c3>img.item(x,y,2):
+            c3=img.item(x,y,2)
+        if d3<img.item(x,y,2):
+            d3=img.item(x,y,2)
 
 for x in range(a):
     for y in range(b):
-        
-        x1=abs(math.floor(img.item(x,y,0)*(C))  +  math.floor(img2.item(x,y,0)*(1-C)))
-        img.itemset((x,y,0),x1)
-        x1=abs(math.floor(img.item(x,y,1)*(C))  +  math.floor(img2.item(x,y,1)*(1-C)))
-        img.itemset((x,y,1),x1)
-        x1=abs(math.floor(img.item(x,y,2)*(C))  +  math.floor(img2.item(x,y,2)*(1-C)))
-        img.itemset((x,y,2),x1)
-        
+        t=(img.item(x,y,0)-c1)*((B-A)/(d1-c1))+A
+        img.itemset((x,y,0),t)
+        t=(img.item(x,y,1)-c2)*((B-A)/(d2-c2))+A
+        img.itemset((x,y,1),t)
+        t=(img.item(x,y,2)-c3)*((B-A)/(d3-c3))+A
+        img.itemset((x,y,2),t)
+
+
 cv2.imshow('asd',img)
-cv2.imwrite('flashbacks.jpg',img)
+cv2.imwrite('se_movio.jpg',img)
